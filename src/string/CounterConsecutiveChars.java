fa5757c3e788ae314c247690915272973558a59a
kristinagerdt <gerdt.kristina@gmail.com>
2020-01-29 20:20:01 +0100

    add fixes with String

--- src/string/CounterConsecutiveChars.java
+++ src/string/CounterConsecutiveChars.java
@@ -1,15 +1,20 @@
 package string;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class CounterConsecutiveChars {
     public static void main(String[] args) {
-//        String str = "aaaabbaacccb";
-        String str = "cccb";
-        System.out.println(str);
-        String result = counter(str);
-        System.out.println(result);
+        String input = "aaaabbaacccb";
+        //String input = "cccb";
+        System.out.println(input);
+        System.out.println(counter(input));
+        System.out.println(counterStream(input));
     }
 
-    public static String counter(String input) {
+    private static String counter(String input) {
         int count = 1;
         StringBuilder output = new StringBuilder();
         for (int i = 0; i < input.length() - 1; i++) {
@@ -21,6 +26,23 @@ public class CounterConsecutiveChars {
             }
         }
         output.append(count).append(input.charAt(input.length() - 1));
+        return output.toString();
+    }
+
+    private static String counterStream(String input) {
+        List<Character> characters = input
+                .chars()
+                .mapToObj(c -> (char) c)
+                .collect(Collectors.toList());
+        Set<Character> uniqueChars = new HashSet<>(characters);
+        StringBuilder output = new StringBuilder();
+        for (Character c : uniqueChars) {
+            long count = characters
+                    .stream()
+                    .filter(c::equals)
+                    .count();
+            output.append(count).append(c);
+        }
         return output.toString();
     }
 }
